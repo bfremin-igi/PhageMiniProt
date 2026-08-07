74c353b37ae1b15a219eb7e83f0960da712304ea
Brayon Fremin <bfremin@berkeley.edu>
2026-08-07 13:09:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="PhageMiniProt", 
-    version="0.1.0",  # Increment version appropriately
+    name="PhageMiniProt",
+    version="0.2.0",  # Bumped: adds `train` CLI command, holdout eval, bundled model data
     author="Brayon Fremin",
     author_email="bfremin@berkeley.edu",
     description="A tool for classifying phage proteins using MiniProt embeddings.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bfremin-igi/PhageMiniProt", 
+    url="https://github.com/bfremin-igi/PhageMiniProt",
     packages=find_packages(),  # Automatically find package directories
+    include_package_data=True,
+    package_data={
+        # Model directories now live inside the package (phageminiprot/models/...)
+        # so they ship with `pip install .` instead of only existing in the repo.
+        "phageminiprot": [
+            "models/PhageMiniProt_model/*",
+            "models/MetaMiniProt_model/*",
+        ],
+    },
     entry_points={
         "console_scripts": [
             "PhageMiniProt=phageminiprot.main:main",  # Main entry point
@@ -34,7 +43,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
